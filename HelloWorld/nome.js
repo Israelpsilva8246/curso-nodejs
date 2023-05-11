f7bdf3684f44d7fed661949df8f3969fd4f98e9b
Israel Silva <israel.silva@phoebus.local>
2023-05-11 08:02:20 -0300

    first commit

---
+++ HelloWorld/nome.js
@@ -0,0 +1,4 @@
+var nome = "Israel"
+var sobrenome = "Pontes"
+
+console.log(nome + " " + sobrenome);
---
+++ modulos/calc.js
@@ -0,0 +1,7 @@
+var SomaFunc = require("./somar")
+var SubFunc = require("./subtrair")
+var MultFunc = require("./multiplicar")
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(1,2));
+console.log(MultFunc(100,10));
